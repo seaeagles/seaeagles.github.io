4e278cef1153315f4e9e562e276601c0a9cef964
David M <dminerur@outlook.com>
2023-03-29 23:06:23 -0400

    consolidated generateSkills function

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// Generate skills in work experience 
 
-function generateSkills1() {
-    const jobCard1 = document.getElementById("jobcard1");
-    const skills = ["", "TEM/MDM application support", "Microsoft SQL(SMSS)", "JIRA, Zendesk, Azure DevOps", "Product Implementation", "QA testing" ];
+function generateSkills(jobCardId) {
+    const jobCard = document.getElementById(jobCardId);
+    const skills = (jobCardId === "jobcard1") 
+      ? ["", "TEM/MDM application support", "Microsoft SQL(SMSS)", "JIRA, Zendesk, Azure DevOps", "Product Implementation", "QA testing"]
+      : ["", "SMTP, TCP/IP protocols", "DNS: SPF/DKIM/DMARC", "Firewalls, web security", "Network troubleshooting", "Linux (bash)/CLI"];
 
-    if (jobCard1.disabled) {
+    if (jobCard.disabled) {
         alert("Error! Only one click per visit, please.");
         return;
       }
     
-      jobCard1.disabled = true;
+      jobCard.disabled = true;
 
     const skillsList = document.createElement("ul");
 
@@ -18,30 +21,7 @@ function generateSkills1() {
     skillsList.appendChild(listItem);
     });
 
-        jobCard1.appendChild(skillsList)
+        jobCard.appendChild(skillsList)
     }
-
-
-function generateSkills2 () {
-    const jobCard2 = document.getElementById("jobcard2");
-    const skills = ["", "SMTP, TCP/IP protocols", "DNS: SPF/DKIM/DMARC", "Firewalls, web security", "Network troubleshooting", "Linux (bash)/CLI"];
-
-    if (jobCard2.disabled) {
-        alert("Error! Only one click per visit, please.");
-        return;
-      }
-    
-      jobCard2.disabled = true;
-
-    const skillsList = document.createElement("ul");
-
-    skills.forEach((skill) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = skill;
-        skillsList.appendChild(listItem);
-    });
-
-    jobCard2.appendChild(skillsList);
- }
 
 
